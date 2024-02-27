860ddf52af3046dd7d7edbd3e80e37f459a34490
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-02-26 23:53:59 -0300

    Adding example with API parameters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 # Response codes simplified
 # 1XX - Hold on
 # 2XX - Here you go
@@ -6,7 +7,7 @@ import requests
 # 4XX - You screwed up
 # 5XX - I screwed up (some server issue)
 
-
+# Simple example of API without parameters
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
 
@@ -15,4 +16,21 @@ latitude = response.json()["iss_position"]["latitude"]
 
 
 print(f"{latitude} {longitude} ")
+
+# Example with parameters
+parameters = {
+    "lat": "-29.6604",
+    "lng": "-51.0562",
+    "formatted": "0",
+    "tzid": "America/Sao_Paulo"
+}
+response = requests.get(url=f"https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+sunrise = response.json()["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = response.json()["results"]["sunset"].split("T")[1].split(":")[0]
+print(f"{sunrise} {sunset}")
+
+
+time_now = datetime.now()
+print(time_now.hour)
 
